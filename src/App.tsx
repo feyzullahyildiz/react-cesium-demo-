8cfa52d8768b94b48c5926207798e893216d29d4
Feyzullah YILDIZ <ahmet.feyzullah.yildiz@gmail.com>
2022-02-02 16:13:30 +0300

    timeline falan kaldırıldı

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,20 @@ import './App.css';
 
 function App() {
   useEffect(() => {
-    const viewer = new Cesium.Viewer('map');
-    
+    const viewer = new Cesium.Viewer('map', {
+      timeline: false,
+      geocoder: false,
+      homeButton: false,
+      infoBox: false,
+      // vrButton: true,
+      animation: false,
+      navigationHelpButton: false,
+      // baseLayerPicker: false,
+      sceneModePicker: false,
+      fullscreenButton: false,
+      selectionIndicator: false,
+    });
+    (viewer as any)._cesiumWidget._creditContainer.style.display = "none";
     return () => {
       viewer.destroy();
     }
