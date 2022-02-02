3675a7c485467ff3c37b8ad69025c7c6584c93eb
Feyzullah YILDIZ <ahmet.feyzullah.yildiz@gmail.com>
2022-02-02 16:37:59 +0300

    2 tane tileset eklendi.

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,31 @@ function App() {
       selectionIndicator: false,
     });
     (viewer as any)._cesiumWidget._creditContainer.style.display = "none";
+
+    const tileset = new Cesium.Cesium3DTileset({
+      url: 'https://cesium-api.ankageo.com/api/v1/static/tileset/3/tileset.json',
+      maximumScreenSpaceError: 1,
+    });
+    viewer.scene.primitives.add(tileset);
+    const tileset2 = new Cesium.Cesium3DTileset({
+      url: 'https://cesium-api.ankageo.com/api/v1/static/tileset/14/tileset.json',
+      maximumScreenSpaceError: 1,
+    });
+    viewer.scene.primitives.add(tileset2);
+    viewer.zoomTo(tileset as any);
+
+    tileset.readyPromise.then(() => {
+      const x = 0;
+      const y = 0;
+      const z = -55;
+      const boundingSphere = tileset.boundingSphere;
+      const cartographic = Cesium.Cartographic.fromCartesian(boundingSphere.center);
+      const surface = Cesium.Cartesian3.fromRadians(cartographic.longitude, cartographic.latitude, 0.0);
+      const offset = Cesium.Cartesian3.fromRadians(cartographic.longitude + x, cartographic.latitude + y, z);
+      const translation = Cesium.Cartesian3.subtract(offset, surface, new Cesium.Cartesian3());
+      tileset.modelMatrix = Cesium.Matrix4.fromTranslation(translation);
+    })
+    // viewer.scene
     return () => {
       viewer.destroy();
     }
