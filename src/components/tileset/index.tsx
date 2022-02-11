f58055db9189bf1b627c58c8f1e51ae816402613
Noor Ahmad <n.ahmad0093@gmail.com>
2022-02-11 11:08:38 +0300

    new enitity

--- src/components/tileset/index.tsx
+++ src/components/tileset/index.tsx
@@ -1,40 +1,41 @@
-import React, { useContext, useEffect, useRef } from 'react';
-import { ViewerContext } from '../viewer-context';
+import React, { useContext, useEffect, useRef } from "react"
+import { ViewerContext } from "../viewer-context"
 
 interface Props {
-    url: string;
-    visible?: boolean;
-    autoZoom?: boolean;
+	url: string
+	visible?: boolean
+	autoZoom?: boolean
 }
 export const Tileset: React.FC<Props> = ({ url, visible = true, autoZoom = false, ...props }) => {
-    const viewer = useContext(ViewerContext);
-    const tileset = useRef<Cesium.Cesium3DTileset | null>(null);
-    // console.log('viewer from tileset', viewer);
-    // console.log('props.url', props.url);
-    useEffect(() => {
-        const t = new Cesium.Cesium3DTileset({
-            url
-        });
-        // (t as any).show = visible;
-        tileset.current = t;
-        // console.log('tileset oluştu');
-        viewer!.scene.primitives.add(t);
-        return () => {
-            // console.log('eski tileset öldü')
-            viewer!.scene.primitives.remove(t);
-        }
-    }, [viewer, url]);
-    useEffect(() => {
-        if(autoZoom) {
-            const t = tileset.current! as any;
-            viewer!.zoomTo(t);
-        }
-    }, [autoZoom]);
+	const viewer = useContext(ViewerContext)
+	const tileset = useRef<Cesium.Cesium3DTileset | null>(null)
+	// console.log('viewer from tileset', viewer);
+	// console.log('props.url', props.url);
+	useEffect(() => {
+		const t = new Cesium.Cesium3DTileset({
+			url,
+			maximumScreenSpaceError: 1,
+		})
+		// (t as any).show = visible;
+		tileset.current = t
+		// console.log('tileset oluştu');
+		viewer!.scene.primitives.add(t)
+		return () => {
+			// console.log('eski tileset öldü')
+			viewer!.scene.primitives.remove(t)
+		}
+	}, [viewer, url])
+	useEffect(() => {
+		if (autoZoom) {
+			const t = tileset.current! as any
+			viewer!.zoomTo(t)
+		}
+	}, [autoZoom])
 
-    useEffect(() => {
-        const t = tileset.current! as any;
-        t.show = visible;
-        // console.log('visible değişti');
-    }, [visible]);
-    return <div></div>;
-};
+	useEffect(() => {
+		const t = tileset.current! as any
+		t.show = visible
+		// console.log('visible değişti');
+	}, [visible])
+	return <div></div>
+}
--- src/components/viewer/index.tsx
+++ src/components/viewer/index.tsx
@@ -1,46 +1,43 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { ViewerContext } from '../viewer-context';
+import React, { useEffect, useRef, useState } from "react"
+import { ViewerContext } from "../viewer-context"
 
 export const Viewer: React.FC = (props) => {
-    const mapRef = useRef(document.createElement('div'));
-    const [viewer, setViewer] = useState<Cesium.Viewer | null>(null);
+	const mapRef = useRef(document.createElement("div"))
+	const [viewer, setViewer] = useState<Cesium.Viewer | null>(null)
 
-    useEffect(() => {
-        const v = new Cesium.Viewer(
-            mapRef.current, {
-                // animation: false,
-                homeButton: false,
-                navigationHelpButton: false,
-                infoBox: false,
-                geocoder: false,
-                baseLayerPicker: false,
-                sceneModePicker: false,
-                fullscreenButton: false,
-                selectionIndicator: false,
-                // timeline: false,
-                navigationInstructionsInitiallyVisible: false,
-                imageryProvider: new Cesium.UrlTemplateImageryProvider({
-                    url: 'https://mt3.google.com/vt/lyrs=s@113&hl=tr&x={x}&y={y}&z={z}',
-                    maximumLevel: 22,
-                    minimumLevel: 1
-                })
-            }
-        );
+	useEffect(() => {
+		const v = new Cesium.Viewer(mapRef.current, {
+			// animation: false,
+			homeButton: false,
+			navigationHelpButton: false,
+			infoBox: false,
+			geocoder: false,
+			baseLayerPicker: false,
+			sceneModePicker: false,
+			fullscreenButton: false,
+			selectionIndicator: false,
+			// timeline: false,
+			navigationInstructionsInitiallyVisible: false,
+			imageryProvider: new Cesium.UrlTemplateImageryProvider({
+				url: "https://mt3.google.com/vt/lyrs=s@113&hl=tr&x={x}&y={y}&z={z}",
+				maximumLevel: 22,
+				minimumLevel: 1,
+			}),
+		})
 
-        // (v as any)._cesiumWidget._creditContainer.style.display = "none";
-        (window as any).viewer = v;
-        setViewer(v);
-        return () => {
-            v.destroy();
-        }
-    }, []);
-    return <>
-        <div style={{ height: '100%' }} ref={mapRef}></div>
-        {
-            viewer &&
-            <ViewerContext.Provider value={viewer}>
-                {props.children}
-            </ViewerContext.Provider>
-        }
-    </>;
-};
+		// (v as any)._cesiumWidget._creditContainer.style.display = "none";
+		;(window as any).viewer = v
+		setViewer(v)
+		return () => {
+			v.destroy()
+		}
+	}, [])
+	return (
+		<>
+			<div style={{ height: "100%" }} ref={mapRef}></div>
+			{viewer && (
+				<ViewerContext.Provider value={viewer}>{props.children}</ViewerContext.Provider>
+			)}
+		</>
+	)
+}
